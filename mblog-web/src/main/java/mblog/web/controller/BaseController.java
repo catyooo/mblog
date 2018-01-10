10da52fed7b551b7d5298725d07b2fa7797cae27
zhangchaoToHub <35050758+zhangchaoToHub@users.noreply.github.com>
2018-01-10 17:02:15 +0800

    BaseController代码格式优化

--- mblog-web/src/main/java/mblog/web/controller/BaseController.java
+++ mblog-web/src/main/java/mblog/web/controller/BaseController.java
@@ -37,7 +37,6 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -45,105 +44,104 @@ import java.util.List;
 
 /**
  * Controller 基类
- * 
+ *
  * @author langhsu
- * 
  */
 public class BaseController {
-	@Autowired
-	protected HttpSession session;
-	@Autowired
-	protected AppContext appContext;
-	@Autowired
-	protected FileRepoFactory fileRepoFactory;
+    @Autowired
+    protected HttpSession session;
+    @Autowired
+    protected AppContext appContext;
+    @Autowired
+    protected FileRepoFactory fileRepoFactory;
 
-	@InitBinder
-	public void initBinder(ServletRequestDataBinder binder) {
-		/**
-		 * 自动转换日期类型的字段格式
-		 */
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
+    @InitBinder
+    public void initBinder(ServletRequestDataBinder binder) {
+        /**
+         * 自动转换日期类型的字段格式
+         */
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
 
-		/**
-		 * 防止XSS攻击
-		 */
-		binder.registerCustomEditor(String.class, new StringEscapeEditor(true, false));
-	}
+        /**
+         * 防止XSS攻击
+         */
+        binder.registerCustomEditor(String.class, new StringEscapeEditor(true, false));
+    }
 
-	/**
-	 * 获取登录信息
-	 * 
-	 * @return
-	 */
-	protected AccountSubject getSubject(){
-	    return (AccountSubject) SecurityUtils.getSubject();
-	}
-	
-	protected void putProfile(AccountProfile profile) {
-		SecurityUtils.getSubject().getSession(true).setAttribute("profile", profile);
-	}
+    /**
+     * 获取登录信息
+     *
+     * @return
+     */
+    protected AccountSubject getSubject() {
+        return (AccountSubject) SecurityUtils.getSubject();
+    }
 
-	protected AuthenticationToken createToken(String username, String password) {
-		return new UsernamePasswordToken(username, MD5.md5(password));
-	}
+    protected void putProfile(AccountProfile profile) {
+        SecurityUtils.getSubject().getSession(true).setAttribute("profile", profile);
+    }
 
-	protected Pageable wrapPageable() {
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-		int pageSize = ServletRequestUtils.getIntParameter(request, "pageSize", 10);
-		int pageNo = ServletRequestUtils.getIntParameter(request, "pn", 1);
-		return new PageRequest(pageNo - 1, pageSize);
-	}
+    protected AuthenticationToken createToken(String username, String password) {
+        return new UsernamePasswordToken(username, MD5.md5(password));
+    }
 
-	/**
-	 * 包装分页对象
-	 *
-	 * @param pn 页码
-	 * @param pn 页码
-	 * @return
-	 */
-	protected Pageable wrapPageable(Integer pn, Integer pageSize) {
-		if (pn == null || pn == 0) {
-			pn = 1;
-		}
-		if (pageSize == null || pageSize == 0) {
-			pageSize = 10;
-		}
-		return new PageRequest(pn - 1, pageSize);
-	}
+    protected Pageable wrapPageable() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        int pageSize = ServletRequestUtils.getIntParameter(request, "pageSize", 10);
+        int pageNo = ServletRequestUtils.getIntParameter(request, "pn", 1);
+        return new PageRequest(pageNo - 1, pageSize);
+    }
 
-	protected String getSuffix(String name) {
-		int pos = name.lastIndexOf(".");
-		return name.substring(pos);
-	}
+    /**
+     * 包装分页对象
+     *
+     * @param pn 页码
+     * @param pn 页码
+     * @return
+     */
+    protected Pageable wrapPageable(Integer pn, Integer pageSize) {
+        if (pn == null || pn == 0) {
+            pn = 1;
+        }
+        if (pageSize == null || pageSize == 0) {
+            pageSize = 10;
+        }
+        return new PageRequest(pn - 1, pageSize);
+    }
 
-	protected String getView(String view) {
-		return "/default" + view;
-	}
-	
-	protected String routeView(String route, String group) {
-		String format = "/default" + route;
-		return String.format(format, group);
-	}
+    protected String getSuffix(String name) {
+        int pos = name.lastIndexOf(".");
+        return name.substring(pos);
+    }
 
-	public static String getIpAddr(HttpServletRequest request) throws Exception {
-		String ip = request.getHeader("X-Real-IP");
-		if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-			return ip;
-		}
-		ip = request.getHeader("X-Forwarded-For");
-		if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-			// 多次反向代理后会有多个IP值，第一个为真实IP。
-			int index = ip.indexOf(',');
-			if (index != -1) {
-				return ip.substring(0, index);
-			} else {
-				return ip;
-			}
-		} else {
-			return request.getRemoteAddr();
-		}
-	}
-	
+    protected String getView(String view) {
+        return "/default" + view;
+    }
+
+    protected String routeView(String route, String group) {
+        String format = "/default" + route;
+        return String.format(format, group);
+    }
+
+    public static String getIpAddr(HttpServletRequest request) throws Exception {
+        String ip = request.getHeader("X-Real-IP");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        ip = request.getHeader("X-Forwarded-For");
+        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个IP值，第一个为真实IP。
+            int index = ip.indexOf(',');
+            if (index != -1) {
+                return ip.substring(0, index);
+            } else {
+                return ip;
+            }
+        } else {
+            return request.getRemoteAddr();
+        }
+    }
+
 //	protected List<Attach> handleAlbums(String[] albums) {
 //		if (albums == null || albums.length == 0) {
 //			return Collections.emptyList();
@@ -185,32 +183,32 @@ public class BaseController {
 //		return rets;
 //	}
 
-	protected void extractImages(Post post) {
-		Document doc = Jsoup.parse(post.getContent());
-		Elements elements = doc.select("img");
-		List<Attach> rets = new ArrayList<>();
+    protected void extractImages(Post post) {
+        Document doc = Jsoup.parse(post.getContent());
+        Elements elements = doc.select("img");
+        List<Attach> rets = new ArrayList<>();
 
-		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 
-		for (Element el : elements) {
-			String imageUrl = el.attr("src");
+        for (Element el : elements) {
+            String imageUrl = el.attr("src");
 
-			if (request.getContextPath().length() > 1 && imageUrl.startsWith(request.getContextPath())) {
-				imageUrl = imageUrl.replace(request.getContextPath(), "");
-			}
-			Attach a = new Attach();
-			a.setOriginal(imageUrl);
-			a.setPreview(imageUrl);
+            if (request.getContextPath().length() > 1 && imageUrl.startsWith(request.getContextPath())) {
+                imageUrl = imageUrl.replace(request.getContextPath(), "");
+            }
+            Attach a = new Attach();
+            a.setOriginal(imageUrl);
+            a.setPreview(imageUrl);
 
-			if (imageUrl.startsWith("http")) {
-				a.setStore(1);
-			}
-			rets.add(a);
-		}
+            if (imageUrl.startsWith("http")) {
+                a.setStore(1);
+            }
+            rets.add(a);
+        }
 
-		post.setContent(doc.html());
-		if (rets.size() > 0) {
-			post.setAlbums(rets);
-		}
-	}
+        post.setContent(doc.html());
+        if (rets.size() > 0) {
+            post.setAlbums(rets);
+        }
+    }
 }
